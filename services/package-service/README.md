c74e08e55a32aa9cefc9d825e2bd135e13f09312
Vaikunthan Paramananthasivam <40295408+Vaikunthan28@users.noreply.github.com>
2025-04-22 19:35:22 +1000

    Add files via upload

---
+++ services/package-service/README.md
@@ -0,0 +1,7 @@
+# Package Service
+
+This is the Package Service for the Smart Delivery Tracker.
+
+### Endpoints:
+- GET /health
+- GET /package/<id>
--- services/package-service/app.py
+++ services/package-service/app.py
@@ -1,1 +1,18 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "OK"})
+
+@app.route('/package/<id>')
+def get_package(id):
+    return jsonify({
+        "package_id": id,
+        "status": "In Transit",
+        "eta": "2 hours"
+    })
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
